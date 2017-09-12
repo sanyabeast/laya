b8cc1c3d3bf16d7a57a3cf8b6701c0cf306d2d36
sanyabeast <a.gvrnsk@gmail.com>
2017-09-12 14:50:08 +0300

    collection proto

--- src/Laya/modules/Util.js
+++ src/Laya/modules/Util.js
@@ -27,6 +27,9 @@ define(function(){
 			if (isArray){
 				for (var a = 0, l = target.length; a < l; a++){
 					callback.call(context, target[a], a);
+					if (l != target.length){
+						return this._iterate.apply(this, arguments);
+					}
 				}
 			} else {
 				for (var k in target){
@@ -38,6 +41,36 @@ define(function(){
 		},
 		iterate : function(callback, context){
 			this._iterate(this.content, this.isArray, callback, context);
+		},
+		contains : function(checkValue){
+			var result = false;
+
+			this.iterate(function(value){
+				if (value === checkValue){
+					result = true;
+				}
+			}, this);
+
+			return result;
+
+		},
+		add : function(key, value){
+			if (this.isArray){
+				this._content.push(key);
+			} else {
+				this._content[key] = value;
+			}
+		},
+		remove : function(key){
+			if (this.isArray){
+				this.iterate(function(value, index){
+					if (value === key){
+						this._content.splice(index, 1);
+					}
+				});
+			} else {
+				delete this._content[key];
+			}
 		}
 	};
 
