ed3322e00282a17d57b124b6d067509882ba4de7
sanyabeast <a.gvrnsk@gmail.com>
2018-03-14 12:28:37 +0200

    Mutator: wrong parameters checking

--- src/Laya/modules/Mutator.js
+++ src/Laya/modules/Mutator.js
@@ -2,6 +2,10 @@
 define(function(){
 
 	var Mutator = function(element, callback){
+		if (!(element instanceof window.Element) || typeof callback != "function"){
+			return;
+		}
+		
  		var observer = new MutationObserver(callback);
 		var config = { attributes: true, childList: true, characterData: true };
 		observer.observe(element, config);
